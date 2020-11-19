66b49da8a2ebe2da001132fd54bfa23384b6898f
crovelli <crovelli@gmail.com>
2020-11-19 11:50:29 +0100

    lowPt with PFoverlap studies added

--- macros/prepareInputsFromNaniInMc.C
+++ macros/prepareInputsFromNaniInMc.C
@@ -19,7 +19,7 @@ using namespace std;
 // mainly to compare the kinematics with and without TnP selection
 // Select signal and background based on match with MC-truth
 
-void prepareInputsFromNaniInMc::Loop(int testLowPt)
+void prepareInputsFromNaniInMc::Loop(int testLowPt, bool studyOverlap)
 {
   Long64_t nentries = fChain->GetEntriesFast();
   Long64_t nbytes = 0, nb = 0;
@@ -38,6 +38,31 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
   ptFakeMc    -> Sumw2();    
   etaSignalMc -> Sumw2();    
   etaFakeMc   -> Sumw2();    
+  // 
+  TH1F *mvaSignalMc_LptPfOverlap    = new TH1F("mvaSignalMc_LptPfOverlap",    "mvaSignalMc_LptPfOverlap",    60, -10., 10.); 
+  TH1F *ptSignalMc_LptPfOverlap     = new TH1F("ptSignalMc_LptPfOverlap",     "ptSignalMc_LptPfOverlap",     60,   0., 15.);
+  TH1F *etaSignalMc_LptPfOverlap    = new TH1F("etaSignalMc_LptPfOverlap",    "etaSignalMc_LptPfOverlap",    40, -2.4, 2.4);
+  TH1F *mvaSignalMc_LptNotPfOverlap = new TH1F("mvaSignalMc_LptNotPfOverlap", "mvaSignalMc_LptNotPfOverlap", 60, -10., 10.); 
+  TH1F *ptSignalMc_LptNotPfOverlap  = new TH1F("ptSignalMc_LptNotPfOverlap",  "ptSignalMc_LptNotPfOverlap",  60,   0., 15.);
+  TH1F *etaSignalMc_LptNotPfOverlap = new TH1F("etaSignalMc_LptNotPfOverlap", "etaSignalMc_LptNotPfOverlap", 40, -2.4, 2.4);
+  TH1F *mvaFakeMc_LptPfOverlap      = new TH1F("mvaFakeMc_LptPfOverlap",      "mvaFakeMc_LptPfOverlap",      60, -10., 10.); 
+  TH1F *ptFakeMc_LptPfOverlap       = new TH1F("ptFakeMc_LptPfOverlap",       "ptFakeMc_LptPfOverlap",       60,   0., 15.);
+  TH1F *etaFakeMc_LptPfOverlap      = new TH1F("etaFakeMc_LptPfOverlap",      "etaFakeMc_LptPfOverlap",      40, -2.4, 2.4);
+  TH1F *mvaFakeMc_LptNotPfOverlap   = new TH1F("mvaFakeMc_LptNotPfOverlap",   "mvaFakeMc_LptNotPfOverlap",   60, -10., 10.); 
+  TH1F *ptFakeMc_LptNotPfOverlap    = new TH1F("ptFakeMc_LptNotPfOverlap",    "ptFakeMc_LptNotPfOverlap",    60,   0., 15.);
+  TH1F *etaFakeMc_LptNotPfOverlap   = new TH1F("etaFakeMc_LptNotPfOverlap",   "etaFakeMc_LptNotPfOverlap",   40, -2.4, 2.4);
+  mvaSignalMc_LptPfOverlap->Sumw2();  
+  ptSignalMc_LptPfOverlap->Sumw2();
+  etaSignalMc_LptPfOverlap->Sumw2();  
+  mvaSignalMc_LptNotPfOverlap->Sumw2();  
+  ptSignalMc_LptNotPfOverlap->Sumw2();
+  etaSignalMc_LptNotPfOverlap->Sumw2();  
+  mvaFakeMc_LptPfOverlap->Sumw2();  
+  ptFakeMc_LptPfOverlap->Sumw2();
+  etaFakeMc_LptPfOverlap->Sumw2();  
+  mvaFakeMc_LptNotPfOverlap->Sumw2();  
+  ptFakeMc_LptNotPfOverlap->Sumw2();
+  etaFakeMc_LptNotPfOverlap->Sumw2();  
   //
   TH1F *etaSignalMc0 = new TH1F("etaSignalMc0", "etaSignalMc0", 40, -2.4, 2.4);
   TH1F *etaFakeMc0   = new TH1F("etaFakeMc0",   "etaFakeMc0",   40, -2.4, 2.4);
@@ -141,6 +166,9 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
       if (testLowPt==1 && Electron_isLowPt[iEle]==0) continue;
       if (testLowPt==0 && Electron_isPF[iEle]==0) continue;
 
+      // Sanity check  
+      if (testLowPt==0 && studyOverlap==1) { cout << "should not happen!" << endl; continue; }
+
       // Acceptance
       if (fabs(Electron_eta[iEle])>2.4) continue;
       if (Electron_pt[iEle]<0.5)        continue;
@@ -170,6 +198,20 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
 	ptSignalMc->Fill(Electron_pt[iEle]);
 	etaSignalMc->Fill(Electron_eta[iEle]);
 	ptVsEtaSignalMc->Fill(Electron_eta[iEle],Electron_pt[iEle]);
+	// 
+	// LPT / PF overlap study
+	if (studyOverlap==1) {
+	  if (Electron_isLowPt[iEle]==1 && Electron_isPFoverlap[iEle]==1) {
+	    mvaSignalMc_LptPfOverlap -> Fill(Electron_mvaId[iEle]);
+	    ptSignalMc_LptPfOverlap  -> Fill(Electron_pt[iEle]);
+	    etaSignalMc_LptPfOverlap -> Fill(Electron_eta[iEle]);
+	  } 
+	  if (Electron_isLowPt[iEle]==1 && Electron_isPFoverlap[iEle]==0) {
+	    mvaSignalMc_LptNotPfOverlap -> Fill(Electron_mvaId[iEle]);
+	    ptSignalMc_LptNotPfOverlap  -> Fill(Electron_pt[iEle]);
+	    etaSignalMc_LptNotPfOverlap -> Fill(Electron_eta[iEle]);
+	  }
+	}
 	//
 	if (Electron_pt[iEle]<1.0) etaSignalMc0->Fill(Electron_eta[iEle]); 
 	if (Electron_pt[iEle]>=1.0 && Electron_pt[iEle]<1.5) etaSignalMc1->Fill(Electron_eta[iEle]); 
@@ -182,6 +224,20 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
 	ptFakeMc->Fill(Electron_pt[iEle]);
 	etaFakeMc->Fill(Electron_eta[iEle]);
 	ptVsEtaFakeMc->Fill(Electron_eta[iEle],Electron_pt[iEle]);
+	//
+	// LPT / PF overlap study
+	if (studyOverlap==1) {
+	  if (Electron_isLowPt[iEle]==1 && Electron_isPFoverlap[iEle]==1) {
+	    mvaFakeMc_LptPfOverlap -> Fill(Electron_mvaId[iEle]);
+	    ptFakeMc_LptPfOverlap  -> Fill(Electron_pt[iEle]);
+	    etaFakeMc_LptPfOverlap -> Fill(Electron_eta[iEle]);
+	  } 
+	  if (Electron_isLowPt[iEle]==1 && Electron_isPFoverlap[iEle]==0) {
+	    mvaFakeMc_LptNotPfOverlap -> Fill(Electron_mvaId[iEle]);
+	    ptFakeMc_LptNotPfOverlap  -> Fill(Electron_pt[iEle]);
+	    etaFakeMc_LptNotPfOverlap -> Fill(Electron_eta[iEle]);
+	  }
+	}
 	//
 	if (Electron_pt[iEle]<1.0) etaFakeMc0->Fill(Electron_eta[iEle]); 
 	if (Electron_pt[iEle]>=1.0 && Electron_pt[iEle]<1.5) etaFakeMc1->Fill(Electron_eta[iEle]); 
@@ -250,6 +306,31 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
   etaSignalMc -> SetLineColor(6);
   etaFakeMc   -> SetLineWidth(2);
   etaFakeMc   -> SetLineColor(4);
+  //
+  mvaSignalMc_LptPfOverlap -> SetLineWidth(2);  
+  mvaSignalMc_LptPfOverlap -> SetLineColor(6);  
+  mvaFakeMc_LptPfOverlap   -> SetLineWidth(2);  
+  mvaFakeMc_LptPfOverlap   -> SetLineColor(6);  
+  ptSignalMc_LptPfOverlap  -> SetLineWidth(2);  
+  ptSignalMc_LptPfOverlap  -> SetLineColor(6);  
+  ptFakeMc_LptPfOverlap    -> SetLineWidth(2);  
+  ptFakeMc_LptPfOverlap    -> SetLineColor(6);  
+  etaSignalMc_LptPfOverlap -> SetLineWidth(2);  
+  etaSignalMc_LptPfOverlap -> SetLineColor(6);  
+  etaFakeMc_LptPfOverlap   -> SetLineWidth(2);  
+  etaFakeMc_LptPfOverlap   -> SetLineColor(6);  
+  mvaSignalMc_LptNotPfOverlap -> SetLineWidth(2);  
+  mvaSignalMc_LptNotPfOverlap -> SetLineColor(4);  
+  mvaFakeMc_LptNotPfOverlap   -> SetLineWidth(2);  
+  mvaFakeMc_LptNotPfOverlap   -> SetLineColor(4);  
+  ptSignalMc_LptNotPfOverlap  -> SetLineWidth(2);  
+  ptSignalMc_LptNotPfOverlap  -> SetLineColor(4);  
+  ptFakeMc_LptNotPfOverlap    -> SetLineWidth(2);  
+  ptFakeMc_LptNotPfOverlap    -> SetLineColor(4);  
+  etaSignalMc_LptNotPfOverlap -> SetLineWidth(2);  
+  etaSignalMc_LptNotPfOverlap -> SetLineColor(4);  
+  etaFakeMc_LptNotPfOverlap   -> SetLineWidth(2);  
+  etaFakeMc_LptNotPfOverlap   -> SetLineColor(4);  
   //
   mvaSignalEBMc0 -> SetLineWidth(2); 
   mvaSignalEBMc1 -> SetLineWidth(2); 
@@ -338,6 +419,18 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
   mvaFakeEEMc2   -> Write();
   mvaFakeEEMc3   -> Write();
   mvaFakeEEMc4   -> Write();
+  mvaSignalMc_LptPfOverlap -> Write();
+  ptSignalMc_LptPfOverlap  -> Write();
+  etaSignalMc_LptPfOverlap -> Write();
+  mvaSignalMc_LptNotPfOverlap -> Write();
+  ptSignalMc_LptNotPfOverlap  -> Write();
+  etaSignalMc_LptNotPfOverlap -> Write();
+  mvaFakeMc_LptPfOverlap -> Write();
+  ptFakeMc_LptPfOverlap  -> Write();
+  etaFakeMc_LptPfOverlap -> Write();
+  mvaFakeMc_LptNotPfOverlap -> Write();
+  ptFakeMc_LptNotPfOverlap  -> Write();
+  etaFakeMc_LptNotPfOverlap -> Write();
   //
   myfile.Close();
 
@@ -526,5 +619,97 @@ void prepareInputsFromNaniInMc::Loop(int testLowPt)
   mvaFakeEEMc4->DrawNormalized("samehist");
   leg->Draw();
   cmvaee4.SaveAs("outputBDT_matchVsFake_noTnP_EE4.png");  
+  //
+  //
+  if (studyOverlap==1) {
+    
+    TLegend *legA1;
+    legA1 = new TLegend(0.10,0.65,0.45,0.90);
+    legA1->SetFillStyle(0);
+    legA1->SetBorderSize(0);
+    legA1->SetTextSize(0.05);
+    legA1->SetFillColor(0);
+    legA1->AddEntry(mvaFakeMc_LptNotPfOverlap, "No PF overlap", "lp");
+    legA1->AddEntry(mvaFakeMc_LptPfOverlap,    "PF overlap", "lp");
+    //
+    TLegend *legB1;
+    legB1 = new TLegend(0.40,0.65,0.75,0.90);
+    legB1->SetFillStyle(0);
+    legB1->SetBorderSize(0);
+    legB1->SetTextSize(0.05);
+    legB1->SetFillColor(0);
+    legB1->AddEntry(mvaFakeMc_LptNotPfOverlap, "No PF overlap", "lp");
+    legB1->AddEntry(mvaFakeMc_LptPfOverlap,    "PF overlap", "lp");
+    //
+    TLegend *legC1;
+    legC1 = new TLegend(0.25,0.15,0.65,0.30);
+    legC1->SetFillStyle(0);
+    legC1->SetBorderSize(0);
+    legC1->SetTextSize(0.05);
+    legC1->SetFillColor(0);
+    legC1->AddEntry(mvaFakeMc_LptNotPfOverlap, "No PF overlap", "lp");
+    legC1->AddEntry(mvaFakeMc_LptPfOverlap,    "PF overlap", "lp");
+
+    TCanvas ca1("ca1","ca1",1);
+    mvaFakeMc_LptNotPfOverlap -> SetTitle(""); 
+    mvaFakeMc_LptPfOverlap    -> SetTitle(""); 
+    mvaFakeMc_LptNotPfOverlap -> GetXaxis()->SetTitle("Id BDT");
+    mvaFakeMc_LptPfOverlap    -> GetXaxis()->SetTitle("Id BDT");
+    mvaFakeMc_LptNotPfOverlap -> DrawNormalized("hist");
+    mvaFakeMc_LptPfOverlap    -> DrawNormalized("samehist");
+    legA1->Draw();
+    ca1.SaveAs("outputBDT_fakeMC_w-wo-overlap.png");
+    //
+    TCanvas ca1b("ca1b","ca1b",1);
+    mvaSignalMc_LptNotPfOverlap -> SetTitle(""); 
+    mvaSignalMc_LptPfOverlap    -> SetTitle(""); 
+    mvaSignalMc_LptNotPfOverlap -> GetXaxis()->SetTitle("Id BDT");
+    mvaSignalMc_LptPfOverlap    -> GetXaxis()->SetTitle("Id BDT");
+    mvaSignalMc_LptPfOverlap    -> DrawNormalized("hist");
+    mvaSignalMc_LptNotPfOverlap -> DrawNormalized("samehist");
+    legA1->Draw();
+    ca1b.SaveAs("outputBDT_matchMC_w-wo-overlap.png");
+
+    TCanvas ca2("ca2","ca2",1);
+    ptFakeMc_LptNotPfOverlap -> SetTitle(""); 
+    ptFakeMc_LptPfOverlap    -> SetTitle(""); 
+    ptFakeMc_LptNotPfOverlap -> GetXaxis()->SetTitle("pt [GeV]");
+    ptFakeMc_LptPfOverlap    -> GetXaxis()->SetTitle("pt [GeV]");
+    ptFakeMc_LptNotPfOverlap -> DrawNormalized("hist");
+    ptFakeMc_LptPfOverlap    -> DrawNormalized("samehist");
+    legB1->Draw();
+    ca2.SaveAs("pt_fakeMC_w-wo-overlap.png");
+    //
+    TCanvas ca2b("ca2b","ca2b",1);
+    ptSignalMc_LptNotPfOverlap -> SetTitle(""); 
+    ptSignalMc_LptPfOverlap    -> SetTitle(""); 
+    ptSignalMc_LptNotPfOverlap -> GetXaxis()->SetTitle("pt [GeV]");
+    ptSignalMc_LptPfOverlap    -> GetXaxis()->SetTitle("pt [GeV]");
+    ptSignalMc_LptNotPfOverlap -> DrawNormalized("hist");
+    ptSignalMc_LptPfOverlap    -> DrawNormalized("samehist");
+    legB1->Draw();
+    ca2b.SaveAs("pt_matchMC_w-wo-overlap.png");
+
+    TCanvas ca3("ca3","ca3",1);
+    etaFakeMc_LptNotPfOverlap -> SetTitle(""); 
+    etaFakeMc_LptPfOverlap    -> SetTitle(""); 
+    etaFakeMc_LptNotPfOverlap -> GetXaxis()->SetTitle("#eta");
+    etaFakeMc_LptPfOverlap    -> GetXaxis()->SetTitle("#eta");
+    etaFakeMc_LptNotPfOverlap -> DrawNormalized("hist");
+    etaFakeMc_LptPfOverlap    -> DrawNormalized("samehist");
+    legC1->Draw();
+    ca3.SaveAs("eta_fakeMC_w-wo-overlap.png");
+    //
+    TCanvas ca3b("ca3b","ca3b",1);
+    etaSignalMc_LptNotPfOverlap -> SetTitle(""); 
+    etaSignalMc_LptPfOverlap    -> SetTitle(""); 
+    etaSignalMc_LptNotPfOverlap -> GetXaxis()->SetTitle("#eta");
+    etaSignalMc_LptPfOverlap    -> GetXaxis()->SetTitle("#eta");
+    etaSignalMc_LptPfOverlap    -> DrawNormalized("hist");
+    etaSignalMc_LptNotPfOverlap -> DrawNormalized("samehist");
+    legC1->Draw();
+    ca3b.SaveAs("eta_matchMC_w-wo-overlap.png");
+  }  
+
 
 }
--- macros/prepareInputsFromNaniInMc.h
+++ macros/prepareInputsFromNaniInMc.h
@@ -853,7 +853,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop(int testLowPt);
+   virtual void     Loop(int testLowPt, bool studyOverlap=0);
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 };
